87b3df629f8bf3fe0ca5714aeffe0308c7f6dc8d
Qadeer84 <95993252+Qadeer84@users.noreply.github.com>
2022-07-26 20:56:34 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { } from 'react-native'
+import SecondNetworkDeskScreen from './src/screen/SecondNetworkDeskScreen/index'
+import LoginWithEmailScreen from './src/screen/LoginWithEmailScreen'
+import LoginScreen from './src/screen/LoginScreen'
+import ResetPasswordScreen from './src/screen/ResetPasswordScreen'
+import RegisterScreen from './src/screen/RegisterScreen'
+import PhoneVerifyScreen from './src/screen/PhoneVerifyScreen'
+import VerifyCodeScreen from './src/screen/VerifyCodeScreen'
+import HelloNextScreen from './src/screen/HelloNextScreen'
+import BusinessNextScreen from './src/screen/BusinessNextScreen'
+import AreaExpertiseScreen from './src/screen/AreaExpertiseScreen'
+import AddServiceScreen from './src/screen/AddServiceScreen'
+import AddYourServiceScreen from './src/screen/AddYourServiceScreen'
+import AboutServiceScreen from './src/screen/AboutServiceScreen'
+import AddProfilepicScreen from './src/screen/AddProfilePicScreen'
+import MainHomeScreen from './src/screen/MainHomeScreen'
+import SearchNetworkDeskcreen from './src/screen/SearchNetworkDeskScreen'
+import FoundNetworkDeskScreen from './src/screen/FoundNetworkDeskScreen'
+import MessageScreen from './src/screen/MessageScreen'
+import Navigation from './src/Navigation'
+
+const App=() => { 
+   return (
+    < Navigation />
+     
+    ) 
+   }
+   export default App
